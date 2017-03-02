4ce09ccca92fae7bd3d864924cc79a872ae27deb
Raymond Kao <rakao@microsoft.com>
2017-03-01 21:31:08 -0500

    adding terraform example template to create ARM resource group and storage account

--- terraform.tf
+++ terraform.tf
@@ -1,1 +1,39 @@
-terraform.tf
+variable "azure_subscription_id" {
+  type = "string"
+}
+
+variable "azure_tenant_id" {
+  type = "string"
+}
+
+variable "azure_client_id" {
+  type = "string"
+}
+
+variable "azure_client_secret" {
+  type = "string"
+}
+
+provider "azurerm" {
+  subscription_id = "${var.azure_subscription_id}"
+  tenant_id       = "${var.azure_tenant_id}"
+  client_id       = "${var.azure_client_id}"
+  client_secret   = "${var.azure_client_secret}"
+}
+
+resource "azurerm_resource_group" "test" {
+    name     = "terraformtest"
+    location = "Canada Central"
+}
+
+resource "azurerm_storage_account" "testsa" {
+    name = "virtualmachinetestsa"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+
+    location = "${azurerm_resource_group.test.location}"
+    account_type = "Premium_LRS"
+
+    tags {
+        environment = "staging"
+    }
+}
